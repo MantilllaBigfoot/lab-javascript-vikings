04e3efb620a88b3bbe902c08b0e53dc57f4d3b43
Christoph Lotz <c.lotz@medat.de>
2022-02-10 14:21:34 +0100

    finish bonuses

--- src/viking.js
+++ src/viking.js
@@ -61,9 +61,62 @@ class War {
   addSaxon(saxon) {
     this.saxonArmy.push(new Saxon(saxon.health, saxon.strength));
   }
-  vikingAttack() {}
-  saxonAttack() {}
-  showStatus() {}
+  vikingAttack() {
+    let saxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+    let viking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+    let returnValue = '';
+    returnValue = saxon.receiveDamage(viking.strength);
+    if(saxon.health <= 0){
+      let index = this.saxonArmy.indexOf(saxon);
+      this.saxonArmy.splice(index, 1);
+    }
+    return returnValue;
+  }
+  saxonAttack() {
+    let saxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+    let viking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+    let returnValue = '';
+    returnValue = viking.receiveDamage(saxon.strength);
+    console.log(viking.health);
+    if(viking.health <= 0){
+      let index = this.vikingArmy.indexOf(viking);
+      this.vikingArmy.splice(index, 1);
+    }
+    return returnValue;
+  }
+
+  //SUPER BONUS
+  soldierAttack(warrior){
+    let returnValue = ``;
+    if(warrior instanceof Saxon){
+      returnValue = saxon.receiveDamage(viking.strength);
+      if(saxon.health <= 0){
+        let index = this.saxonArmy.indexOf(saxon);
+        this.saxonArmy.splice(index, 1);
+      }
+    }
+    else if(warrior instanceof Viking){
+      returnValue = viking.receiveDamage(saxon.strength);
+      if(viking.health <= 0){
+        let index = this.vikingArmy.indexOf(viking);
+        this.vikingArmy.splice(index, 1);
+      }
+    }
+    return returnValue;
+
+  }
+
+  showStatus() {
+    if(this.saxonArmy.length === 0){
+      return `Vikings have won the war of the century!`
+    }
+    else if (this.vikingArmy.length === 0){
+      return `Saxons have fought for their lives and survived another day...`
+    }
+    else{
+      return `Vikings and Saxons are still in the thick of battle.`
+    }
+  }
 }
 
 // The following is required to make unit tests work.
